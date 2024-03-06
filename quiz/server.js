a1fcc775569a8bb42301260cbbd9a9302953691c
sristika <borasristika.new@gmail.com>
2024-03-06 14:16:52 -0500

    updates

--- quiz/server.js
+++ quiz/server.js
@@ -6,33 +6,32 @@ var cors = require('cors');
 const port = 8000;
 
 let users;
-fs.readFile(path.resolve(__dirname, '../data/users.json'), function(err, data) {
-  console.log('reading file ... ');
-  if(err) throw err;
-  users = JSON.parse(data);
-})
+fs.readFile(
+  path.resolve(__dirname, '../data/users.json'),
+  function (err, data) {
+    console.log('reading file ... ');
+    if (err) throw err;
+    users = JSON.parse(data);
+  }
+);
 
 const addMsgToRequest = function (req, res, next) {
-  if(users) {
+  if (users) {
     req.users = users;
     next();
-  }
-  else {
+  } else {
     return res.json({
-        error: {message: 'users not found', status: 404}
+      error: { message: 'users not found', status: 404 },
     });
   }
-  
-}
+};
 
-app.use(
-  cors({origin: 'http://localhost:3000'})
-);
+app.use(cors({ origin: 'http://localhost:3000' }));
 app.use('/read/usernames', addMsgToRequest);
 
 app.get('/read/usernames', (req, res) => {
-  let usernames = req.users.map(function(user) {
-    return {id: user.id, username: user.username};
+  let usernames = req.users.map(function (user) {
+    return { id: user.id, username: user.username };
   });
   res.send(usernames);
 });
@@ -44,13 +43,31 @@ app.use('/write/adduser', addMsgToRequest);
 app.post('/write/adduser', (req, res) => {
   let newuser = req.body;
   req.users.push(newuser);
-  fs.writeFile(path.resolve(__dirname, '../data/users.json'), JSON.stringify(req.users), (err) => {
-    if (err) console.log('Failed to write');
-    else console.log('User Saved');
-  });
+  fs.writeFile(
+    path.resolve(__dirname, '../data/users.json'),
+    JSON.stringify(req.users),
+    (err) => {
+      if (err) console.log('Failed to write');
+      else console.log('User Saved');
+    }
+  );
   res.send('done');
-})
+});
+
+app.use('/read/username', addMsgToRequest);
+
+app.get('/read/username/:name', (req, res) => {
+  let userSearch = req.params.name;
+  const foundUser = req.users.filter(
+    (user) => user.username.toLowerCase() === userSearch.toLowerCase()
+  );
+  if (foundUser.length > 0) {
+    res.json(foundUser);
+  } else {
+    return res.json({ error: 'User not found' });
+  }
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
